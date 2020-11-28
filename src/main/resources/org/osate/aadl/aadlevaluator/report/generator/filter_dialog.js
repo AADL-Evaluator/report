d9b3638efae8065cc8dc1fb113beb550363f74b7
avld <avld@avld-pc>
2020-11-27 21:52:46 -0300

    Atualizacao do filtro no relatorio html.

--- src/main/resources/org/osate/aadl/aadlevaluator/report/generator/filter_dialog.js
+++ src/main/resources/org/osate/aadl/aadlevaluator/report/generator/filter_dialog.js
@@ -1,122 +1,91 @@
 Vue.component( 'filter_dialog' , {
-    template : `<div class="modal is-active">
-  <div class="modal-background"></div>
-  <div class="modal-card">
-    <header class="modal-card-head">
-      <p class="modal-card-title">Filter</p>
-      <button class="delete" aria-label="close" @click="closed"></button>
-    </header>
+    template : `
+    <b-modal v-model="mostrar">
+        <div class="p-6" style="background: white;">
+            <h1 class="title">Filter</h1>
 
-    <section class="modal-card-body">
-
-        <div class="field is-horizontal">
-            <div class="field-label is-small">
-                <label class="label">Factor</label>
-            </div>
-            <div class="field-body">
-                <div class="field">
-                    <div class="control">
-                        <select class="select" v-model="selected">
+            <div class="columns">
+                <div class="column">
+                    <b-field label="Attribute">
+                        <b-select size="is-small" placeholder="Select the attribute" v-model="selected">
                             <optgroup v-for="(value, name) in groups" :key="name" :label="name">
                                 <option v-for="item in value" 
                                     :key="item.value" 
                                     :value="item.value">{{ item.title }}</option>
                             </optgroup>
-                        </select>
-                    </div>
+                        </b-select>
+                    </b-field>
                 </div>
+
+                <div class="column">
+                    <b-field size="is-small" label="Slider Range" v-if="info">
+                        <b-select size="is-small" placeholder="Select the limit" v-model="sliderRange">
+                            <option value="0">Limit defined</option>
+                            <option value="1">Value found</option>
+                        </b-select>
+                    </b-field>
+                </div>
+
+                <div class="column">
+                    <b-field size="is-small" label="Min" v-if="info">
+                        <b-input size="is-small" 
+                            placeholder="min value" 
+                            style="text-align: right;"
+                            v-model="min"
+                            expanded
+                        ></b-input>
+                        <p class="control">
+                            <span class="button is-static is-small">{{ info.unit }}</span>
+                        </p>
+                    </b-field>
+                </div>
+
+                <div class="column">
+                    <b-field size="is-small" label="Max" v-if="info">
+                        <b-input size="is-small" 
+                            placeholder="max value" 
+                            style="text-align: right;" 
+                            v-model="max"
+                            expanded
+                        ></b-input>
+                        <p class="control">
+                            <span class="button is-static is-small">{{ info.unit }}</span>
+                        </p>
+                    </b-field>
+                </div>
+            </div>
+
+            <div class="columns">
+                <div class="column p-6">
+                    <my-range 
+                        title="Range"
+                        v-if="info"
+                        :value_min="min" 
+                        :value_max="max"
+                        :range="range" 
+                        :unit="info.unit" 
+                        @input="setValues"
+                    />
+                </div>
+            </div>
+
+            <div class="buttons">
+                <b-button @click="saved" type="is-link">Save</b-button>
+                <b-button @click="closed">Cancel</b-button>
+                <b-button @click="removed" type="is-danger" v-if="edit.index >= 0">Delete</b-button>
             </div>
         </div>
-
-        <div v-if="selected !== ALL_IN_LIMIT">
-            <div class="field is-horizontal">
-                <div class="field-label is-small">
-                    <label class="label">Value Min</label>
-                </div>
-                <div class="field-body">
-                    <div class="field has-addons">
-                        <div class="control">
-                            <input v-model="min" class="input is-small" style="text-align: right;" type="text" placeholder="0">
-                        </div>
-                        <div class="control">
-                            <a v-if="info && info.unit" class="button is-static is-small">{{ info.unit }}</a>
-                            <a v-else class="button is-static is-small"></a>
-                        </div>
-                    </div>
-
-                    <p class="help">
-                        <div v-if="info" class="field is-grouped is-grouped-multiline">
-                            <div class="control">
-                                <div class="tags has-addons">
-                                    <span class="tag is-dark">Value</span>
-                                    <span class="tag is-info">{{ info.valueMin }}</span>
-                                </div>
-                            </div>
-
-                            <div class="control">
-                                <div class="tags has-addons">
-                                    <span class="tag is-dark">Limit</span>
-                                    <span class="tag is-danger">{{ info.limitMin }}</span>
-                                </div>
-                            </div>
-                        </div>
-                    </p>
-                </div>
-            </div>
-
-            <div class="field is-horizontal">
-                <div class="field-label is-small">
-                    <label class="label">Value Max</label>
-                </div>
-                <div class="field-body">
-                    <div class="field has-addons">
-                        <div class="control">
-                            <input v-model="max" class="input is-small" style="text-align: right;" type="text" placeholder="10">
-                        </div>
-                        <div class="control">
-                            <a v-if="info && info.unit" class="button is-static is-small">{{ info.unit }}</a>
-                            <a v-else class="button is-static is-small"></a>
-                        </div>
-                    </div>
-
-                    <p class="help">
-                        <div v-if="info" class="field is-grouped is-grouped-multiline">
-                            <div class="control">
-                                <div class="tags has-addons">
-                                    <span class="tag is-dark">Value</span>
-                                    <span class="tag is-info">{{ info.valueMax }}</span>
-                                </div>
-                            </div>
-
-                            <div class="control">
-                                <div class="tags has-addons">
-                                    <span class="tag is-dark">Limit</span>
-                                    <span class="tag is-danger">{{ info.limitMax }}</span>
-                                </div>
-                            </div>
-                        </div>
-                    </p>
-                </div>
-            </div>
-        </div>
-
-    </section>
-
-    <footer class="modal-card-foot">
-        <button class="button is-success" @click="saved">Save</button>
-        <button class="button" @click="closed">Cancel</button>
-        <button v-if="edit.index >= 0" class="button is-danger" @click="removed">Deleted</button>
-    </footer>
-  </div>
-</div>
+    </b-modal>
     ` ,
 
     props : [ "edit" ] ,
 
     data : function (){
         return {
+            mostrar : true ,
             groups : getItemsGroup() ,
             selected : "" ,
+            sliderRange : 0 ,
             min : 0 ,
             max : 0 ,
             ALL_IN_LIMIT : ALL_IN_LIMIT
@@ -135,10 +104,25 @@ Vue.component( 'filter_dialog' , {
             }
 
             return getResume( this.selected );
+        } ,
+
+        range : function (){
+            if( !isSet( this.info ) ){
+                return { min : 0 , max : 0 };
+            }
+
+            return Number( this.sliderRange ) == 0
+                ? { min : this.info.limitMin , max : this.info.limitMax }
+                : { min : this.info.valueMin , max : this.info.valueMax };
         }
     } ,
 
     methods : {
+        setValues : function ( values ){
+            this.min = values[0];
+            this.max = values[1];
+        } ,
+
         saved : function(){
             this.$emit( 'saved' , {
                 index : this.edit.index ,
@@ -162,6 +146,12 @@ Vue.component( 'filter_dialog' , {
         }
     } ,
 
+    watch : {
+        mostrar : function ( v ){
+            this.closed();
+        }
+    } ,
+
     mounted : function (){
         this.selected = this.edit.factor;
         this.min = this.edit.min;
@@ -170,5 +160,76 @@ Vue.component( 'filter_dialog' , {
 
     destroyed : function (){
         
+    }
+} );
+
+
+
+Vue.component( 'my-range' , {
+    template : `
+    <b-field :label="title">
+        <b-slider 
+            :value="value"
+            @input="event"
+            size="is-small"
+            :min="range.min" 
+            :max="range.max"
+            :custom-formatter="formatter"
+            :tooltip="true" 
+            :step="step"
+            tooltip-always
+        ></b-slider>
+    </b-field>
+    ` ,
+
+    props : [ "title" , "value_min" , "value_max" , "unit" , "range" ] ,
+
+    data : function (){
+        return {
+            value : [ 0 , 0 ]
+        };
+    } ,
+
+    watch : {
+        value_min : function (){
+            this.value = [ 
+                this.value_min , 
+                this.value_max 
+            ];
+        } ,
+
+        value_max : function (){
+            this.value = [ 
+                this.value_min , 
+                this.value_max 
+            ];
+        } ,
+    } ,
+
+    computed : {
+        step : function (){
+            return (this.range.max - this.range.min) / 10;
+        }
+    } ,
+
+    methods : {
+        formatter : function ( value ){
+            return this.unit 
+                ? `${value.toPrecision(4)} ${this.unit}`
+                : `${value}`;
+        } ,
+
+        event : function ( value ){
+            if( isSet( value ) ){
+                this.$emit( 'input' , value );
+            }
+        }
+    } ,
+
+    created : function (){
+        this.value = [ 
+            this.value_min , 
+            this.value_max 
+        ];
     }
 } );
